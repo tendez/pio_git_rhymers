99b6b900746add85c4987180d5e38ca213135b7b
MaciejKrakowiak <240719@edu.p.lodz.pl>
2023-04-18 23:59:57 +0200

    7. dodanie final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
     public static final int MAX_SIZE = 12;
     public static final int INT = -1;
-    private int[] numbers = new int[MAX_SIZE];
+    private final int[] numbers = new int[MAX_SIZE];
 
     public int total = -1;
 
